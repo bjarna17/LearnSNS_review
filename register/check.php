0d8ffa8e7cc04d88bfc5c08864f2a8e5b0e21c67
西村結有 <bjarna.17@gmail.com>
2018-11-10 18:49:03 +0800

    きちんと動作しているのか確認しました

--- register/check.php
+++ register/check.php
@@ -1,4 +1,10 @@
 <?php
+    session_start();
+
+    echo $_SESSION['register']['name'].'<br>';
+    echo $_SESSION['register']['email'].'<br>';
+    echo $_SESSION['register']['password'].'<br>';
+    echo $_SESSION['register']['img_name'].'<br>';
 ?>
 <!DOCTYPE html>
 <html>
@@ -6,6 +12,6 @@
   <title></title>
 </head>
 <body>
-
+  <img src="../user_profile_img/<?php echo $_SESSION['register']['img_name']; ?>" width="60">
 </body>
 </html>
