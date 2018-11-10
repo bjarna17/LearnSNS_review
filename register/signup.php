dfc2555bbcca9ae0d618979528cab6a16b44c98a
西村結有 <bjarna.17@gmail.com>
2018-11-10 11:19:31 +0800

    初めてのコミット assetsを追加しました
    作るサイトのデザイン性を高めるためです

---
+++ register/signup.php
@@ -0,0 +1,45 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>LearnSNS</title>
+  <link rel="stylesheet" type="text/css" href="href=../assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+</head>
+<body style="margin-top: 60px">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offest-2 thumbnail">
+        <h2 class="text-center content_header">アカウント作成</h2>
+        <form method="POST" action="signup.php" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="name">ユーザー名</label>
+            <input type="text" name="input_name" class="form-control" id="name" placeholder="山田太郎">
+          </div>
+          <div class="form-group">
+            <label for="email">メールアドレス</label>
+            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+          </div>
+          <div class="form-group">
+            <label for="password">パスワード</label>
+            <input type="password" name="input_password" class="form-control" id="password" placeholder="4~16文字のパスワード">
+          </div>
+           <div class="form-group">
+            <label for="img_name">パスワード</label>
+            <input type="password" name="input_password" class="form-control" id="password" placeholder="4~16文字のパスワード">
+          </div>
+          <input type="submit" class="btn btn-default" value="確認">
+          <a href="../signin.php" style="float: right; padding-top: 6px;" class="text-success">サインイン</a>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/js/jquery-3.1.1.js"></script>
+  <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="../assets/js/bootstrap.js"></script>
+</body>
+</html>
